d2d9c4fa2d07c236f5410f63ce2233a6e05253dd
Hannes <Hannes.Hofer@gmail.com>
2015-02-10 20:03:00 +0100

    add myo data handling class
    myo data handling in its own class.
    gets a callback that triggers when new emg data is available
    myo connection & handling is done in this file/class

---
+++ mymyo.py
@@ -0,0 +1,57 @@
+import myo
+
+
+class MyMyo:
+    def __init__(self, callback):
+        self.hub = None
+        self.myo = None
+        self.init(callback)
+
+    def __del__(self):
+        if self.hub:
+            self.hub.stop(True)
+
+    def init(self, callback):
+        myo.init()
+        self.hub = myo.Hub()
+        self.hub.set_locking_policy(myo.locking_policy.none)
+        self.myo = self.MyoListener(self)
+        self.myo.callback = callback
+        self.hub.run(1000, self.myo)
+
+    class MyoListener(myo.DeviceListener):
+        def __init__(self, managment):
+            self.callback = None
+            self.rssi = None
+            self.connected = False
+            self.paired = False
+            self.synched = False
+            self.managment = managment
+
+        def on_connect(self, myo, timestamp):
+            myo.vibrate('short')
+            self.connected = True
+            myo.request_rssi()
+
+        def on_rssi(self, myo, timestamp, rssi):
+            self.rssi = rssi
+
+        def on_pair(self, myo, timestamp):
+            self.paired = True
+
+        def on_disconnect(self, myo, timestamp):
+            self.connected = False
+
+        def on_sync(self, myo, timestamp, arm, x_direction):
+            self.synched = True
+
+        def on_unsync(self, myo, timestamp):
+            self.synched = False
+
+        def on_emg(self, myo, timestamp, emg):
+            if self.callback:
+                self.callback(emg)
+
+
+if __name__ == '__main__':
+    my = MyMyo()
