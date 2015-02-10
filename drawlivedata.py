7ebb10f5a2f8219e703e64e59cb046fc4e27eec3
Hannes <Hannes.Hofer@gmail.com>
2015-02-10 20:04:45 +0100

    use actual myo data to update graphs
    use myo data to update graphs
    this needs to be threadsafe since we get/set data in diffrent thread
    and call redraw in its own thread.

--- drawlivedata.py
+++ drawlivedata.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtGui
 import sys
-from random import randint
+import pyqtgraph as pg
+from mymyo import MyMyo
+from pyqtgraph.Qt import QtCore, QtGui
+from PyQt4.Qt import QMutex
 
 
 plotters = []
 app = QtGui.QApplication([])
-timer = timer2 = win = None
+timer = myotimer = win = myo = None
 
 
 class PlotImage:
@@ -21,43 +22,46 @@ class PlotImage:
         self._pen = pg.mkPen(color="#3366FF")
         self._plotstart = 0
         self._plot = None
+        self.mutex = QMutex()
 
     def add_data(self, datapoint):
+        self.mutex.lock()
         self._data.append(datapoint)
         if self._plotstart < self.XAXISLENGTH:
             self._data.pop(0)
         else:
             self._plotstart += 1
+        self.mutex.unlock()
 
     def draw(self):
+        self.mutex.lock()
         if not self._curve:
             self._curve = self._plot.plot(pen=self._pen)
         self._curve.setData(x=self.XAXISDATA, y=self._data[self._plotstart:])
+        self.mutex.unlock()
 
     def addplot(self, win):
         self._plot = win.addPlot(title=self._title)
         self._plot.setRange(xRange=(0, self.XAXISLENGTH))
 
 
-def update_plot_data():
-    global plotters
-    for p in plotters:
-        p.add_data(randint(-2000, 2000))
+def update_plot_data(emgdata):
+    for p, e in zip(emgdata, plotters):
+        p.add_data(e)
 
 
 def update_draw():
-    global plotters
     for p in plotters:
         p.draw()
 
 
-def update():
-    update_plot_data()
-    update_draw()
+def startmyo():
+    global myo
+    myo = MyMyo(update_plot_data)
 
 
 def main():
-    global win, timer, timer2
+    global win, timer, myotimer
     win = pg.GraphicsWindow(title="Myo Sensor Data")
     win.resize(1000, 600)
     win.setBackground(None)
@@ -68,13 +72,14 @@ def main():
         plotters.append(PlotImage(str('Sensor %s' % str(int(i) + 1))))
         plotters[i].addplot(win)
 
-    timer = QtCore.QTimer()
-    timer.timeout.connect(update_plot_data)
-    timer.start(10)
+    myotimer = QtCore.QTimer()
+    myotimer.timeout.connect(startmyo)
+    myotimer.setSingleShot(True)
+    myotimer.start(1)
 
-    timer2 = QtCore.QTimer()
-    timer2.timeout.connect(update_draw)
-    timer2.start(100)
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update_draw)
+    timer.start(100)
 
 
 if __name__ == '__main__':
